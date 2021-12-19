7e907b92baae3801f23c740abc3b894be0f1bd33
yb8350 <yb8350@naver.com>
2021-12-20 06:47:53 +0900

    Update 계산기 기능

--- ttt.js
+++ ttt.js
@@ -1,13 +1,25 @@
-const offset = new Date().getTimezoneOffset() * 60000;
-let tempDate = new Date(Date.now() - offset);
+// const offset = new Date().getTimezoneOffset() * 60000;
+// let tempDate = new Date(Date.now() - offset);
 
-let checkDate = "20211218";
-console.log(
-  new Date(
-    checkDate.slice(0, 4),
-    parseInt(checkDate.slice(4, 6)) - 1,
-    checkDate.slice(6, 8)
-  ).getDay()
-);
+// let checkDate = "20211218";
+// console.log(
+//   new Date(
+//     checkDate.slice(0, 4),
+//     parseInt(checkDate.slice(4, 6)) - 1,
+//     checkDate.slice(6, 8)
+//   ).getDay()
+// );
 
-console.log("2,3,5,".slice(0, -1).split(","));
+// console.log("2,3,5,".slice(0, -1).split(","));
+
+// let a = "0",
+//   b = "30",
+//   c = "12",
+//   d = "0";
+
+// let ch = function (n) {
+//   return n.length == 1 ? "0" + n : n + "";
+// };
+// let start_time = ch(ev.start_hour) + ":" + ch(ev.start_minute);
+// let last_time = ch(ev.last_hour) + ":" + ch(ev.last_minute);
+// console.log(start_time, last_time);
